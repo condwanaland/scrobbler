20ed4a4335cc0b61e64fbd46b2c06cee1ce9e917
Conor Neilson <condwanaland@gmail.com>
2018-12-01 15:51:51 +0000

    Split numeric conversion into its own function

--- R/check_args.R
+++ R/check_args.R
@@ -12,3 +12,24 @@ check_args_are_chr <- function(username, out_file, start_page = NULL){
               is.character(start_page) || is.null(start_page))},
     error = function(e) stop("Arguments must be supplied as characters"))
 }
+
+
+
+#' check_start_page_to_numeric
+#'
+#' @param start_page
+#'
+#'@noRd
+#'
+
+check_start_page_to_numeric <- function(start_page){
+  tryCatch({
+    as.integer(start_page)},
+    warning = function(w){
+      stop("start_page could not be converted to a whole number")
+    },
+    error = function(e){
+      stop("start_page could not be converted to a whole number")
+    }
+  )
+}
--- R/fetch_tracks.R
+++ R/fetch_tracks.R
@@ -16,21 +16,14 @@ fetch_tracks <- function(username, out_file, start_page = NULL){
   # Arguments must be quoted - check that everything is a character
   check_args_are_chr(username, out_file, start_page)
 
+  # Check that start_page can be successfully converted to numeric
+  check_start_page_to_numeric(start_page)
+
   # Store args as objects to be used later
   u <- username
   o <- out_file
   s <- start_page
 
-  # Check that start_page can be successfully converted to numeric
-  tryCatch({
-    as.numeric(start_page)},
-    warning = function(w){
-      stop("start_page could not be converted to a whole number")
-    },
-    error = function(e){
-      stop("start_page could not be converted to a whole number")
-    }
-  )
 
   invisible()
 }
