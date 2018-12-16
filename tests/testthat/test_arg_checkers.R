553bc72b38a03818be7124f64ba73ee4f2f7cdea
Conor Neilson <condwanaland@gmail.com>
2018-12-16 18:01:00 +0000

    Update tests for new error message

--- tests/testthat/test_arg_checkers.R
+++ tests/testthat/test_arg_checkers.R
@@ -6,13 +6,13 @@ context("Test the low level arg checkers work")
 
 test_that("Non characters throw error", {
   expect_error(check_args_are_chr("one", "two", 12),
-               regexp = "Arguments must be supplied as characters", fixed = TRUE)
+               regexp = "All arguments must be supplied as characters", fixed = TRUE)
   expect_error(check_args_are_chr(13, "two", 12),
-               regexp = "Arguments must be*")
+               regexp = "All arguments must be*")
   expect_error(check_args_are_chr(13, 15, 12),
-               regexp = "Arguments must be*")
+               regexp = "All arguments must be*")
   expect_error(check_args_are_chr("one", 15, 12),
-               regexp = "Arguments must be*")
+               regexp = "All arguments must be*")
 })
 
 test_that("Characters do not throw errors", {
--- tests/testthat/test_fetch_errors.R
+++ tests/testthat/test_fetch_errors.R
@@ -2,13 +2,13 @@ context("Fetch_tracks error handling")
 
 test_that("All arguments are treated as characters", {
   expect_error(fetch_tracks("one", "two", 12),
-               regexp = "Arguments must be supplied as characters", fixed = TRUE)
+               regexp = "All arguments must be supplied as characters", fixed = TRUE)
   expect_error(fetch_tracks(13, "two", 12),
-               regexp = "Arguments must be*")
+               regexp = "All arguments must be*")
   expect_error(fetch_tracks(13, 15, 12),
-               regexp = "Arguments must be*")
+               regexp = "All arguments must be*")
   expect_error(fetch_tracks("one", 15, 12),
-               regexp = "Arguments must be*")
+               regexp = "All arguments must be*")
 })
 
 
