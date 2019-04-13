cd81e8e61ebcd0448f8a9650c01a8668d4511fa9
Conor Neilson <condwanaland@gmail.com>
2019-04-13 21:42:36 +0100

    finish and test url constructor

---
+++ R/get_pages_api.R
@@ -0,0 +1,126 @@
+#' get_total_pages
+#'
+#' Returns the total number of pages that your song list spans across. Used for determining
+#' how many pages to loop the api call over
+#'
+#' @param username Your `Last.fm` username, as a string
+#' @param api_key Your `Last.fm` api_key, as a string
+#'
+#' @return A single number character string
+#' @noRd
+
+get_total_pages <- function(username, api_key){
+  print("Getting page numbers...")
+  base_url <- paste0(
+    "http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=",
+    username,
+    "&limit=1000&api_key=",
+    api_key,
+    "&format=json"
+  )
+
+  response <- httr::GET(base_url)
+  text_response <- httr::content(response, "text")
+  parsed = fromJSON(text_response)
+
+
+  total_pages = as.integer(parsed[["recenttracks"]][["@attr"]][["totalPages"]])
+  page_1 = parsed[["recenttracks"]][["track"]]
+
+  return(list(total_pages,
+              page_1))
+}
+
+#' construct_urls
+#'
+#' @param total_pages Total number of pages to build
+#' @param username Your username
+#' @param api_key Your api_key
+#'
+#' @return Vector of urls
+#' @noRd
+#'
+
+construct_urls <- function(total_pages, username, api_key){
+  result <- vector("character")
+
+  # Start by looping over 2nd page - 1st page is returned by the get_total_pages function
+  for (page in 2:total_pages){
+    urls <- paste0(
+      "http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=",
+      username,
+      "&limit=1000&api_key=",
+      api_key,
+      "&format=json&page=",
+      page
+    )
+    result[page] <- urls
+  }
+  return(na.omit(result))
+}
+
+
+collect_tracks <- function(username, api_key){
+  #pb <- progress_bar$new(total = 100)
+  #pb$tick(0)
+
+  tracks <- get_total_pages(username, api_key)
+  total_pages = tracks[[1]]
+
+  all_urls <- construct_urls(total_pages, username, api_key)
+
+  ## Pass urls to a get call and parse response
+
+  all_dat <- lapply(all_urls, function(x){
+    httr::GET(x)
+  })
+
+  all_dat <- lapply(all_dat, function(x){
+    httr::content(x, "text")
+  })
+
+  all_dat <- lapply(all_dat, function(x){
+    jsonlite::fromJSON(x, flatten = TRUE)
+  })
+
+  parsed_dat <- lapply(all_dat, function(x){
+    x[["recenttracks"]][["track"]]
+  })
+
+  return(parsed_dat)
+
+}
+
+# data_dat <- lapply(parsed_dat, function(x){
+#   x[["recenttracks"]][["track"]]
+# })
+#
+# new_dat_list <- lapply(data_dat2, function(x) do.call(cbind, x))
+#
+# data2 <- do.call(rbind, new_dat_list)
+#
+# bound_dat <- data.table::rbindlist(data_dat)
+# bound_dat <- dplyr::bind_rows(data_dat)
+#
+# dat1 <- data_dat[[1]]
+# dat2 <- data_dat[[2]]
+# dat3 <- dplyr::bind_rows(dat1, dat2)
+#
+# dat1$image <- NULL
+# dat1$date <- NULL
+# dat1$streamable <- NULL
+#
+# dat2$image <- NULL
+# dat2$date <- NULL
+# dat2$streamable <- NULL
+#
+# row.names(dat1) <- NULL
+# row.names(dat2) <- NULL
+#
+# rbind(dat1, dat2)
+#
+#
+# all_dat_test <- all_dat
+# all_dat_test <- lapply(all_dat_test, function(x){
+#   jsonlite::fromJSON(x, flatten = TRUE)
+# })
---
+++ tests/testthat/test_url_constructor.R
@@ -0,0 +1,21 @@
+context("Test URL constructor")
+
+test_urls <- structure(c("http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=RScrobblerUser&limit=1000&api_key=123456789&format=json&page=2",
+                         "http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=RScrobblerUser&limit=1000&api_key=123456789&format=json&page=3",
+                         "http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=RScrobblerUser&limit=1000&api_key=123456789&format=json&page=4",
+                         "http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=RScrobblerUser&limit=1000&api_key=123456789&format=json&page=5",
+                         "http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=RScrobblerUser&limit=1000&api_key=123456789&format=json&page=6",
+                         "http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=RScrobblerUser&limit=1000&api_key=123456789&format=json&page=7",
+                         "http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=RScrobblerUser&limit=1000&api_key=123456789&format=json&page=8",
+                         "http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=RScrobblerUser&limit=1000&api_key=123456789&format=json&page=9",
+                         "http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=RScrobblerUser&limit=1000&api_key=123456789&format=json&page=10",
+                         "http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=RScrobblerUser&limit=1000&api_key=123456789&format=json&page=11",
+                         "http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=RScrobblerUser&limit=1000&api_key=123456789&format=json&page=12",
+                         "http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=RScrobblerUser&limit=1000&api_key=123456789&format=json&page=13",
+                         "http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=RScrobblerUser&limit=1000&api_key=123456789&format=json&page=14",
+                         "http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=RScrobblerUser&limit=1000&api_key=123456789&format=json&page=15"
+), na.action = structure(1L, class = "omit"))
+
+test_that("constructor correctly creates api links", {
+  expect_equal(test_urls, construct_urls(15, "RScrobblerUser", 123456789))
+})
