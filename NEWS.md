a648dd28f6f5e1a09aa5579cffa6326a763f360f
Conor Neilson <condwanaland@gmail.com>
2020-03-01 17:29:39 +0000

    Update news, add some tests

--- NEWS.md
+++ NEWS.md
@@ -2,6 +2,8 @@
 
 * scrobbler now has better authentication options through environment variables. You can set the 'LASTFM_API_USERNAME' and 'LASTFM_API_KEY' variables to avoid having to pass these everytime. See the README for details.
 
+* Any functions for setting up and running scrobble downloads by using python scripts have been removed. Going via the Last.fm API is now the only officially supported method.
+
 # scrobbler 0.2.15
 
 * Added a `NEWS.md` file to track changes to the package.
--- tests/testthat/test_timestamp_functions.R
+++ tests/testthat/test_timestamp_functions.R
@@ -4,3 +4,14 @@ test_that("last_timestamp throws errors", {
   expect_error(get_last_timestamp(c("Yes", "No")),
                regexp = "`scrobbles_df`` must be a dataframe")
 })
+
+
+timestamp_data <- read.table(system.file("extdata",
+                                         "timestamp_data.txt",
+                                         package = "scrobbler"),
+                             header = TRUE)
+test_that("last timestamp is correctly returned", {
+  expect_equal(get_last_timestamp(timestamp_data, "date_unix"), 1582531414)
+})
+
+rm(timestamp_data)
