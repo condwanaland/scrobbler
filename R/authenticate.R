109226ed86c25fe2dc9de1224271ffe88521dc31
Conor Neilson <condwanaland@gmail.com>
2020-03-01 16:36:18 +0000

    write and test authentication functions

---
+++ R/authenticate.R
@@ -0,0 +1,38 @@
+#' get_lastfm_credentials
+#'
+#' @param env One of 'username' or 'key', to return the username or key environment variable respectively
+#'
+#' @return Character string
+#' @export
+#'
+#' @examples
+#' get_lastfm_credentials(env = 'username')
+#' get_lastfm_credentials(env = 'key')
+get_lastfm_credentials <- function(env){
+
+  if (!env %in% c("username", "key")){
+    stop("env must be one of 'username' or 'key", call. = FALSE)
+  }
+
+  check_env_vars()
+
+  if (env == "username"){
+    val <- Sys.getenv('LASTFM_API_USERNAME')
+  }
+  else if (env == 'key'){
+    val <- Sys.getenv('LASTFM_API_KEY')
+  }
+
+  return(val)
+}
+
+check_env_vars <- function(){
+  username_val <- Sys.getenv('LASTFM_API_USERNAME')
+  key_val <- Sys.getenv('LASTFM_API_KEY')
+
+  if (username_val == "" || key_val == ""){
+    stop("'LASTFM_API_USERNAME' and/or 'LASTFM_API_KEY' environment variables are not set.
+         Set them with 'Sys.setenv() or pass these variables explicitly.", call. = FALSE)
+  }
+  else return(TRUE)
+}
---
+++ tests/testthat/test_authenticate.R
@@ -0,0 +1,19 @@
+context("Testing authentication")
+
+test_that("env vars check throws error", {
+  expect_error(check_env_vars(), "LASTFM_API_USERNAME' and/or 'LASTFM_API_KEY")
+  expect_error(get_lastfm_credentials(env = "myenv"), "env must be one of")
+})
+
+
+Sys.setenv(LASTFM_API_USERNAME = 'test_name')
+Sys.setenv(LASTFM_API_KEY = 'test_key')
+
+test_that("get_creds does not throw error", {
+  expect_equal(get_lastfm_credentials(env = 'username'), 'test_name')
+  expect_equal(get_lastfm_credentials(env = 'key'), 'test_key')
+  expect_equal(check_env_vars(), TRUE)
+})
+
+Sys.unsetenv('LASTFM_API_USERNAME')
+Sys.unsetenv('LASTFM_API_KEY')
